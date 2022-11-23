38934800ff4615c7399730a199896b209a8c5f48
yan <757095197@qq.com>
2022-11-23 10:42:24 +0800

    Initial Commit

---
+++ SwiftUITemplate/SwiftUITemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITemplateApp.swift
+//  SwiftUITemplate
+//
+//  Created by 薛焱 on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
