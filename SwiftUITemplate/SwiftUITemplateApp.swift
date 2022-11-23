ede6a84eebea8e328d19265ef9901f827f30efd9
yan <757095197@qq.com>
2022-11-23 14:15:18 +0800

    first add

--- SwiftUITemplate/SwiftUITemplateApp.swift
+++ SwiftUITemplate/SwiftUITemplateApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct SwiftUITemplateApp: App {
+    @Environment(\.scenePhase) var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { newValue in
+            switch newValue {
+            case .active:
+                print("App is active")
+            case .inactive:
+                print("App is inactive")
+            case .background:
+                print("App is in background")
+            @unknown default:
+                print("Oh - interesting: I received an unexpected new value.")
+            }
         }
     }
 }
---
+++ SwiftUITemplate/TemplateUI.swift
@@ -0,0 +1,20 @@
+//
+//  TemplateUI.swift
+//  SwiftUITemplate
+//
+//  Created by 薛焱 on 2022/11/23.
+//
+
+import SwiftUI
+
+struct TemplateUI: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TemplateUI_Previews: PreviewProvider {
+    static var previews: some View {
+        TemplateUI()
+    }
+}
